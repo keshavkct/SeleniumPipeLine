de2bf3a3e06a2f20643b959f3861c0bc2ec69caa
Joshi <keshav.joshi@cgi.com>
2018-12-17 14:21:52 +0530

    adding Details

--- src/test/java/TestNG/NewTest.java
+++ src/test/java/TestNG/NewTest.java
@@ -1,28 +1,36 @@
 package TestNG;
 
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;		
+	
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;		
 public class NewTest {		
-	    private WebDriver driver;		
-		@Test				
-		public void testEasy() {	
-			driver.get("http://demo.guru99.com/test/guru99home/");  
-			String title = driver.getTitle();				 
-			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
-		}	
-		@BeforeTest
-		public void beforeTest() {	
-			System.setProperty("webdriver.chrome.driver", "c://Softwares//chromedriver.exe");
+	WebDriver wdriver;
+	  @Test
+	  public void f() {
+			wdriver.manage().window().maximize();
+			wdriver.get("http://newtours.demoaut.com");
+			wdriver.findElement(By.linkText("SIGN-ON"));
+			wdriver.findElement(By.name("userName")).sendKeys("testing");
+			wdriver.findElement(By.name("password")).sendKeys("testing");
+			wdriver.findElement(By.name("login")).click();;
+	  }
+	  @BeforeTest
+	  public void beforeTest() {
+		  String absolutePath=System.getProperty("user.dir");
+		  String filePath=absolutePath+"\\chromedriver.exe";
+		  System.setProperty("webdriver.chrome.driver", filePath);
+			//System.setProperty("webdriver.chrome.driver",
+			//		"C:\\WorkSpace\\eclipse-workspace\\Flight_Reservation\\chromedriver.exe");
+			wdriver = new ChromeDriver();
+	  }
 
-			driver = new ChromeDriver();  
-		}		
-		@AfterTest
-		public void afterTest() {
-			driver.quit();			
-		}		
+	  @AfterTest
+	  public void afterTest() {
+		  wdriver.quit();	
+	  }
+		
 }	
